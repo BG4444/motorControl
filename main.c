e20141324d01eba92abe439a7b4bad95573f1fc9
BG <bg4444@list.ru>
2019-01-04 15:00:57 +0300

    blinky mode ok

--- main.c
+++ main.c
@@ -2,11 +2,24 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+
+ISR(TIMER0_OVF_vect)
+{
+    PORTD ^= _BV(PD3);
+}
 
 int main(void)
 {
 
-    // Insert code
+    TCCR0B =1<<CS02 | 1<<CS00;
+    TIMSK  =1<<TOIE0;
+
+    DDRD = _BV(PD3); //_BV(PD4) | _BV(PD5)
+
+    //DDRB = _BV(PB0) | _BV(PB1);
+
+    sei();
 
     while(1)
     ;
